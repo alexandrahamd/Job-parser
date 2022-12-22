4f44ef992eef0af4e040cff7a48951db5c5f10e8
AlexandraHamd <pylina.alexandra@yandex.ru>
2022-12-22 16:27:48 +0300

    add class_connector

---
+++ class_connector.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import json
+
+
+class Connector:
+    """
+    Класс коннектор к файлу
+    """
+    __data_file = None
+
+    def __init__(self, df):
+        self.__data_file = df
+        self.__connect()
+
+    def __connect(self):
+        """
+        Проверка на существование файла с данными и
+        создание его при необходимости
+        """
+        try:
+            if self.__data_file not in os.listdir('.'):
+                with open(self.__data_file, 'w') as file:
+                    file.write(json.dumps([]))
+        except Exception as ex:
+            logging.critical(ex)
+
+    def insert(self, data):
+        try:
+            with open(self.__data_file, 'r+', encoding='utf-8') as f:
+                files = json.load(f)
+                files.append(data)
+
+            with open(self.__data_file, 'w', encoding='utf-8') as f:
+                json.dump(files, f)
+
+        except:
+
+            with open(self.__data_file, 'w', encoding='utf-8') as f:
+                json.dump(data, f)
+
+        return self.__data_file
+
+
+if __name__ == '__main__':
+    df = Connector('df3.json')
+
+    data_for_file = {'id': 1, 'title': 'tet'}
+
+    df.insert(data_for_file)
+    data_from_file = df.select(dict())
+    assert data_from_file == [data_for_file]
+
+    df.delete({'id': 1})
+    data_from_file = df.select(dict())
+    assert data_from_file == []
